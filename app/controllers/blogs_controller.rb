21a6d3f79249e8c2ae7fe02732a635e4e3e41595
tomomiik <tomomiik0210@gmail.com>
2016-09-28 09:29:09 +0000

    dive8更新

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,11 @@
 class BlogsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_blog, only: [:edit, :update, :destroy]
   
   def index
     @blogs=Blog.all
   end
+  
   def new
     if params[:back]
      @blog=Blog.new(blogs_params)
@@ -11,6 +13,7 @@ class BlogsController < ApplicationController
      @blog=Blog.new
     end
   end
+  
   def create
     @blog = Blog.new(blogs_params)
     if @blog.save
@@ -19,8 +22,10 @@ class BlogsController < ApplicationController
       render action: 'new'
     end
   end
+  
   def edit
   end
+  
   def update
     if @blog.update(blogs_params)
       redirect_to blogs_path, notice: "ブログを更新しました！"
@@ -28,18 +33,22 @@ class BlogsController < ApplicationController
       render action: 'edit'
     end
   end
+  
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
   end
+  
   def confirm
     @blog = Blog.new(blogs_params)
     render :new if @blog.invalid?
   end
+  
   private
-  def blogs_params
+   def blogs_params
     params.require(:blog).permit(:title,:content)
-  end
+   end
+   
   def set_blog
     @blog = Blog.find(params[:id])
   end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,3 @@
 class Blog < ActiveRecord::Base
-    validates :title,presence: true
+  validates :title,presence: true
 end
