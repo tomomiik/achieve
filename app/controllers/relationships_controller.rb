636582b7dfa7548e75c4a55f390f28949d1b7149
tomomiik <tomomiik0210@gmail.com>
2016-12-04 07:32:28 +0000

    follow機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,7 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  respond_to :js
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
