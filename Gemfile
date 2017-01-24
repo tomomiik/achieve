4d795004a87433153b04bace7d6c7073453176b3
tomomiik <tomomiik0210@gmail.com>
2017-01-24 12:16:18 +0000

    change storage to fog from file

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'carrierwave'
 gem 'rmagick'
 gem 'pusher'
 gem 'faker'
-
+gem 'fog'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,8 +7,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  # ↓ 以下に変更
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
